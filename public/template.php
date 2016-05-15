c58d8043dc93e2020e45bf282b62bfbd45e03e84
John Bussey <jbussey365@yahoo.com>
2016-05-14 21:06:53 -0400

    fixed path to style sheet in template

--- public/template.php
+++ public/template.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>TicTacTml</title>
-    <link rel="stylesheet" type="text/css" href="../../boards.css">
+    <link rel="stylesheet" type="text/css" href="../../../boards.css">
     <base href="../../">
 </head>
 <body>
